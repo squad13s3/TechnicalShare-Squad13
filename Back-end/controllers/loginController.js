9038f3bf54f286cef8892f4d018931451584aace
Diana Queiroz <dianaqueeiroz@hotmail.com>
2022-04-06 20:00:34 -0400

    estudando e implementando rotas

--- Back-end/controllers/loginController.js
+++ Back-end/controllers/loginController.js
@@ -1,8 +1,10 @@
 module.exports = {
     async login(request, response) {
         //campos preenchidos na hora do login
+     var a=10
+     var b=50
+     var c=a+b
      
-     
-      return response.json("vendedor");
+      return response.json(c);
     }
 }
--- Back-end/src/router.js
+++ Back-end/src/router.js
@@ -5,5 +5,7 @@ const routes = express.Router();
 const loginController = require('../controllers/loginController');
 routes.get('/login', loginController.login);
 
+const registerController=  require ("../controllers/registerController")
+routes.get("/register",registerController.register);
 
 module.exports = routes;
