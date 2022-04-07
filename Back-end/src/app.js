26f0b6096b0f1534d68d6e9fdd226dc91567b502
w <w>
2022-04-07 18:08:58 -0300

    Configuração de cors para nosso ambiente de testes

--- Back-end/src/app.js
+++ Back-end/src/app.js
@@ -3,8 +3,11 @@
 const express = require('express');
 const routes = require('../src/router');
 
+cors = require('cors');
+
 const app = express();
 
+app.use(cors());
 app.use(express.json())
 app.use(routes);
 
