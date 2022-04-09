a373d5949e777b32212a05cb5285369336afe3df
Diana Queiroz <dianaqueeiroz@hotmail.com>
2022-04-09 14:29:30 -0400

    API de login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,9 +1,18 @@
-module.exports = {
+const connection = require('../database/conection')
+
+  module.exports = {
+
     async login(request, response) { 
      const {email, password} = request.body;   
      
-     
-      return response.json(password);
+     const user = await connection('user')
+      .where('email', email)
+      .where('password', password)
+      .select("userId","email","name","lastname")
+      .first()
+  
+
+      return response.json(user);
  
     }
 }
