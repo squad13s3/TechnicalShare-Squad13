4e4e51879c852e0e787009d70a5effebff8a8212
w <w>
2022-04-10 12:12:24 -0300

    dotenv install, mongodb connection

--- src/app.js
+++ src/app.js
@@ -4,8 +4,22 @@ const express = require('express');
 const routes = require('../src/router');
 
 cors = require('cors');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 app.use(cors());
 app.use(express.json())
