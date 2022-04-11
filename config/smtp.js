ed8e7c2ec201c2e9d8d026e4011b87a94b27699c
w <w>
2022-04-11 18:50:21 -0300

    Implementando envio de email para o usuario ao se cadastrar

---
+++ config/smtp.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: "smtp.gmail.com",
+    port: 587,
+    user: "squad13s3@gmail.com",
+    pass:'Squad13@S3'
+}
--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,13 +1,40 @@
 const connection = require('../database/conection')
 const crypto = require('crypto');
 
+const mailer = require('nodemailer')
+const smtpConfiguration = require('../config/smtp');
+const { send } = require('express/lib/response');
+
+const smtpTransport = mailer.createTransport({
+    host: smtpConfiguration.host,
+    port: smtpConfiguration.port,
+    secure: false,
+    auth:{
+        user: smtpConfiguration.user,
+        pass: smtpConfiguration.pass,
+    },
+    tls: {
+        rejectUnauthorized: false,
+      },
+});
+async function sendEmail(email){
+    const mailSent = await smtpTransport.sendMail({
+        text: `Obrigado por se cadastrar na plataforma TechnicalShare, esperamos que aproveite todo aprendizado.` , 
+        subject: "Cadastro Realizado",
+        from: "TechnicalShare contato <squad13s3@gmail.com>",
+        to:[email]
+    });
+    console.log(mailSent)
+}
+
 module.exports = {
     async register (request, response) {
-        const {name,lastname,email, password} = request.body
+        const {name,email, password} = request.body
         const userId = crypto.randomUUID()
-        await connection('user').insert({userId, name,lastname,email,password}) 
-        
-            return response.json({message: 'Usuário cadastrado.' }); 
+        await connection('user').insert({userId, name,email,password}) 
+        sendEmail(email)
+
+        return response.json({message: 'Usuário cadastrado.' }); 
     } 
 }
 
