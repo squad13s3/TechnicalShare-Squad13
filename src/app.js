88479df9f36658ac917731f8646fe705d5200ab7
wolgranjulio <wolgranjulio@gmail.com>
2022-04-13 12:32:30 -0300

    heroku test bug fix

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const routes = require('../src/router');
 
 cors = require('cors');
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 require("dotenv").config();
 //const socket = require("socket.io");
 
