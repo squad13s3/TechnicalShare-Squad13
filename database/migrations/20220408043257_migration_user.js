4ce8a9f77f00aa396393dc29a6376615f6ff3d75
wolgranjulio <wolgranjulio@gmail.com>
2022-04-14 15:04:58 -0300

    inserindo no banco de dados url para imagem de usuario, entretanto para fins demonstrativos

--- database/migrations/20220408043257_migration_user.js
+++ database/migrations/20220408043257_migration_user.js
@@ -7,6 +7,7 @@ exports.up = function(knex) {
         table.string('name').notNullable();
         table.string('about');
         table.string("email").notNullable();
+        table.string("imgUrl");
         table.string("password").notNullable();
         
       
