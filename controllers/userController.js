a2620c2681a756ecdf1750d3077a5db1b86bdf42
wolgranjulio <wolgranjulio@gmail.com>
2022-04-15 12:23:37 -0300

    inserindo try catch para testes com heroku

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const connection = require('../database/conection')
 
   module.exports = { 
     async insertAbout(request, response) { 
+      try {
+        
         const userId= request.params.id
         const {about} = request.body;
 
@@ -10,13 +12,25 @@ const connection = require('../database/conection')
           'about': about
         })
         return response.json({message: 'About atualizado.' });
+     }
+     catch (e) {
+       console.log(e)
+     }
+     
     },
     
     async getAbout(request, response) { 
-      const userId = request.params.id
-     const about = await connection('user').where('userId', userId)
-    .select('about')
-    .first()
-return response.json( about );}
 
-  }
+      try {
+        
+      const userId = request.params.id
+      const about = await connection('user').where('userId', userId)
+        .select('about')
+        .first()
+      return response.json( about );}
+     
+     catch (e) {
+       console.log(e)
+     }
+
+  } }
