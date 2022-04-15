59d7eeaa4526257b3174622006fbf3b216c73e0c
wolgranjulio <wolgranjulio@gmail.com>
2022-04-14 22:50:06 -0300

    refatorando o codigo

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -1,6 +1,43 @@
 const connection = require('../database/conection')
 
   module.exports = {
+    async learnTagUpdate(request, response) { 
+    
+      const userId_FK = request.params.id
+      const {tag_learn1, tag_learn2,tag_learn3} = request.body;
+      
+
+      const learntag = await connection('learn').where('userId_FK', userId_FK)
+      .update({
+        'tag_learn1': tag_learn1, 
+        'tag_learn2': tag_learn2, 
+        'tag_learn3': tag_learn3, 
+      }).select('userId_FK')
+      
+      if (!learntag) {
+        return response.status(400).json({ message: 'Falha ao efetuar update, cheque suas informaçoes' });
+      }
+      
+      return response.json({message: 'tag cadastrada.' }); 
+  },
+  
+  async teachTagUpdate(request, response) { 
+    
+    const userId_FK= request.params.id
+    const {tag_teach1, tag_teach2,tag_teach3} = request.body;
+    
+
+    const teachtag = await connection('teach').where('userId_FK', userId_FK)
+      .update({
+        'tag_teach1': tag_teach1, 
+        'tag_teach2': tag_teach2, 
+        'tag_teach3': tag_teach3, 
+      }).select('userId_FK')
+      if (!teachtag) {
+        return response.status(400).json({ message: 'Falha ao efetuar update, cheque suas informaçoes' });
+      }
+    return response.json({message: 'tag cadastrada.' }); 
+},
 
     async search(request, response) { 
       
--- src/router.js
+++ src/router.js
@@ -5,24 +5,17 @@ const userController = require('../controllers/userController');
 routes.get('/getabout/:id', userController.getAbout)
 routes.post('/about/:id', userController.insertAbout)
 
-
 const authController = require('../controllers/authController');
 routes.post('/login', authController.login);
 routes.post("/register", authController.register);
 
-
-
-const teachTagcontroller = require("../controllers/teachTagController");
-routes.post("/teachtag/:id", teachTagcontroller.create)
-
-const learnTagController = require("../controllers/LearnTagController");
-routes.post("/learntag/:id", learnTagController.create)
-
-const scheduleMentorship = require("../controllers/ScheduleMentorshipController");
-routes.post("/scheduleMentorship/:userId_teacher/:userId_student",scheduleMentorship.create)
+const mentorshipController = require("../controllers/mentorshipController");
+routes.post("/scheduleMentorship/:userId_teacher/:userId_student",mentorshipController.create)
 
 const tagController = require("../controllers/tagController");
 routes.post("/search", tagController.search)
+routes.post("/learntag/:id", tagController.learnTagUpdate)
+routes.post("/teachtag/:id", tagController.teachTagUpdate)
 routes.get("/searchteachtag/:id", tagController.searchTeachTagById)
 routes.get("/searchlearntag/:id", tagController.searchLearnTagById)
 
